783c6fbe54752962a5c24915c72ec0253be2e731
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-07-20 04:45:33 +0530

    Discover UI Complete

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { topics } from "../utils/constants";
+
+const activeTopicStyle =
+  "xl:border-2 hover:bg-primary xl:border-[#6407ff] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#6407ff]";
+const topicStyle =
+  "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
 
 const Discover = () => {
-  return (
-    <div>Discover</div>
-  )
-}
+  const router = useRouter();
+  const { topic } = router.query;
 
-export default Discover
+  return (
+    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+      <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
+        Popular Topics
+      </p>
+      <div className="flex gap-3 flex-wrap">
+        {topics?.map((item) => (
+          <Link href={`/?topic=${item.name}`} key={item.name}>
+            <div
+              className={topic === item.name ? activeTopicStyle : topicStyle}
+            >
+              <span className="font-bold text-2xl xl:text-md ">
+                {item.icon}
+              </span>
+              <span
+                className={`font-medium text-md hidden xl:block capitalize`}
+              >
+                {item.name}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
