cb35d9b7242bfdf31e20b88be926c25e66531ab3
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-07-20 04:19:23 +0530

    Sidebar Complete

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,20 +2,74 @@ import React, { useState } from "react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import GoogleLogin from "@react-oauth/google";
+import GoogleLogin from "react-google-login";
+
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
+import Discover from "./Discover";
+import SuggestedAccounts from "./SuggestedAccounts";
+import Footer from "./Footer";
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
+  const userProfile = false;
+
+  const activeLink =
+    "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#6407ff] rounded";
+
+  const normalLink =
+    "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold rounded";
+
   return (
     <div>
       <div
         className="block xl:hidden m-2 ml-4 mt-3 text-xl"
-        onClick={() => setShowSidebar(!showSidebar)}
+        onClick={() => setShowSidebar((prev) => !prev)}
       >
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
+      {showSidebar && (
+        <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3 ">
+          <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+            <Link href="/">
+              <div className={activeLink}>
+                <p className="text-2xl">
+                  <AiFillHome />
+                </p>
+                <span className="capitalize text-xl hidden xl:block">
+                  For You
+                </span>
+              </div>
+            </Link>
+          </div>
+          {!userProfile && (
+            <div className="px-2 py-4 hidden xl:block">
+              <p className="text-gray-400">login karle</p>
+              <div className="pr-4">
+                <GoogleLogin
+                  clientId=""
+                  render={(renderProps) => (
+                    <button
+                      className=" cursor-pointer bg-white text-lg text-[#6407ff] border-[1px] border-[#6407ff] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#6407ff]"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      Login
+                    </button>
+                  )}
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy="single_host_origin"
+                />
+              </div>
+            </div>
+          )}
+
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
