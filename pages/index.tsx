6fd46a22afc8a650cfc3d7b66cc1f671f54ca0c7
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-07-20 19:08:31 +0530

    Response Success

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import axios from "axios";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+
+export const getServerSideProps = async () => {
+  const response = await axios.get(`http://localhost:3000/api/post`);
+  console.log(response.data.name);
+
+  return {
+    props: {},
+  };
 };
 
 export default Home;
