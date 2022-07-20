dba759446d3380229d34a19bddd180387f36553a
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-07-21 02:12:31 +0530

    upload css complete

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import axios from "axios";
 
-const upload = () => {
-  return <div>upload</div>;
+import useAuthStore from "../store/authStore";
+import { client } from "../utils/client";
+
+// import { SanityAssetDocument } from "@sanity/client";
+
+// import { topics } from "../utils/constants";
+
+const Upload = () => {
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+  const [videoAsset, setVideoAsset] = useState();
+
+  return (
+    <div className="flex w-full h-full absolute left-0 top-[60px] lg:top-[70px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
+      <div className=" bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6">
+        <div>
+          <div>
+            <p className="text-2xl font-bold">Upload Video</p>
+            <p className="text-md text-gray-400 mt-1">
+              Post a video to your account
+            </p>
+          </div>
+          <div className=" border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center  outline-none mt-10 w-[260px] h-[458px] p-10 cursor-pointer hover:border-purple-300 hover:bg-gray-100">
+            {isLoading ? (
+              <p>Uploading...</p>
+            ) : (
+              <div>
+                {videoAsset ? (
+                  <div></div>
+                ) : (
+                  <label className="cursor-pointer">
+                    <div className="flex flex-col items-center justify-center h-full">
+                      <div className="flex flex-col justify-center items-center">
+                        <p className="font-bold text-xl">
+                          <FaCloudUploadAlt className="text-gray-300 text-6xl" />
+                        </p>
+                        <p className="text-xl font-semibold">
+                          Select video to upload
+                        </p>
+                      </div>
+                      <p className="text-gray-400 text-center mt-10 text-sm leading-10">
+                        MP4 or WebM or ogg <br />
+                        720x1280 resolution or higher <br />
+                        Up to 10 minutes <br />
+                        Less than 2 GB
+                      </p>
+                      <p className="bg-[#6407ff] text-center mt-8 rounded text-white text-md font-medium p-2 w-52 outline-none">
+                        Select file
+                      </p>
+                    </div>
+                    <input
+                      type="file"
+                      onChange={(e) => uploadVideo(e)}
+                      name="upload-video"
+                      className="w-0 h-0"
+                    />
+                  </label>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default upload;
+export default Upload;
