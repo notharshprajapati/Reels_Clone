8358ea706101add43a29454029f818346f5acb7b
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-07-20 19:20:58 +0530

    video console log

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import axios from "axios";
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ videos }) => {
+  console.log(videos);
+
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`);
-  console.log(response.data.name);
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
 
   return {
-    props: {},
+    props: {
+      videos: data,
+    },
   };
 };
 
