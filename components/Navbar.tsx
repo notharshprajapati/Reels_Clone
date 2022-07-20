173ba39cea8f863633df45655586e90a3984eceb
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-07-21 01:45:25 +0530

    logout implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
-  const { userProfile, addUser } = useAuthStore();
+  const { userProfile, addUser, removeUser } = useAuthStore();
 
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
@@ -32,7 +32,37 @@ const Navbar = () => {
       <div>Search</div>
       <div>
         {userProfile ? (
-          <div>{userProfile.userName}</div>
+          <div className="flex gap-5 md:gap-10">
+            <Link href="/upload">
+              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" />{" "}
+                <span className="hidden md:block">Upload </span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className=" rounded-full cursor-pointer"
+                    src={userProfile.image}
+                    alt="user-profile"
+                  />
+                </>
+              </Link>
+            )}
+            <button
+              type="button"
+              className=" border-2 p-2 rounded-full cursor-pointer outline-none shadow-md"
+              onClick={() => {
+                googleLogout();
+                removeUser();
+              }}
+            >
+              <AiOutlineLogout color="red" fontSize={21} />
+            </button>
+          </div>
         ) : (
           <GoogleLogin
             onSuccess={(response) => createOrGetUser(response, addUser)}
