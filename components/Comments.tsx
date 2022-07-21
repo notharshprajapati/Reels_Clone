48a91c6b49b8b6f27c7f396c4f8f92de6c049e6f
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-07-22 00:21:13 +0530

    suggested account + comments done

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -79,7 +79,7 @@ const Comments = ({
           <form onSubmit={addComment} className="flex gap-4">
             <input
               value={comment}
-              onChange={(e) => setComment(e.target.value.trim())}
+              onChange={(e) => setComment(e.target.value)}
               className="bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg"
               placeholder="Add comment.."
             />
--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,7 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+
+import { postDetailQuery } from "./../../../utils/queries";
 import { client } from "../../../utils/client";
-import { postDetailQuery } from "../../../utils/queries";
+import { uuid } from "uuidv4";
 
 export default async function handler(
   req: NextApiRequest,
@@ -14,5 +15,23 @@ export default async function handler(
     const data = await client.fetch(query);
 
     res.status(200).json(data[0]);
+  } else if (req.method === "PUT") {
+    const { comment, userId } = req.body;
+
+    const { id }: any = req.query;
+
+    const data = await client
+      .patch(id)
+      .setIfMissing({ comments: [] })
+      .insert("after", "comments[-1]", [
+        {
+          comment,
+          _key: uuid(),
+          postedBy: { _type: "postedBy", _ref: userId },
+        },
+      ])
+      .commit();
+
+    res.status(200).json(data);
   }
 }
